LineageOS/android_device_xiaomi_lithium

lithium: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_lithium.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lithium-user \
+    lineage_lithium-userdebug \
+    lineage_lithium-eng

Inkypen79/platform_device_xiaomi_lithium

AndroidProducts.mk
@@ -13,3 +13,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/syberia_lithium.mk 
+
+COMMON_LUNCH_CHOICES := \
+    syberia_lithium-user \
+    syberia_lithium-userdebug \
+    syberia_lithium-eng