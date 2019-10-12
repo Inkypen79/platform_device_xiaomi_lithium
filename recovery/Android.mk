89fa340a6b2801231e6a00559e14ab010e7cecb9
Nicholas Magill <nmagill1979@gmail.com>
2019-10-13 00:38:17 +0100

    lithium: Drop deprecated eng flags

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,6 +5,6 @@ LOCAL_C_INCLUDES := bootable/recovery \
     bootable/recovery/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_lithium
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
